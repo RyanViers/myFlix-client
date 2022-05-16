3a4085798083e1ccca17af8af1a28b811edd661a
RyanViers <ryanviersiv@hotmail.com>
2022-05-15 22:40:30 -0400

    test

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -13,7 +13,7 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import './main-view.scss';
 
 import { LoginView } from '../login-view/login-view';
-//import { MovieCard } from '../movie-card/movie-card';
+import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { NavbarView } from '../navbar-view/navbar';
 import { RegistrationView } from '../registration-view/registration-view';

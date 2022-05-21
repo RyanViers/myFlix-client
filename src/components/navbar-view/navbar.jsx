5f38cbb50bc72bf19fd13fe7f3db9f9f2d3885fc
RyanViers <ryanviersiv@hotmail.com>
2022-05-21 01:34:27 -0400

    test

--- src/components/navbar-view/navbar.jsx
+++ src/components/navbar-view/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export function NavbarView({ user }) {
   const onLoggedOut = () => {

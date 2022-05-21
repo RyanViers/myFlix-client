b01333ed09d2f78ead0834d85ee9b157e6941a49
RyanViers <ryanviersiv@hotmail.com>
2022-05-21 16:57:41 -0400

    test

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -11,8 +11,7 @@ import { FavoriteMovies } from './favorite-movies';
 import { UpdateUser } from './update-user';
 
 export function ProfileView({ user }) {
-  console.log(user);
-  console.log(props.user.FavoriteMovies);
+  console.log(user.Username);
   //const [userdata, setUserdata] = useState({});
   //const [updatedUser, setUpdatedUser] = useState({});
   //const [favoriteMoviesList, setFavoriteMoviesList] = useState([]);

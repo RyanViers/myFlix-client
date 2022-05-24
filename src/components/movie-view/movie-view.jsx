a373d96a36a514b4eafcfa2c3fba72752188288b
RyanViers <ryanviersiv@hotmail.com>
2022-05-23 23:01:03 -0400

    test

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
 import axios from 'axios';
 import { connect } from 'react-redux';
@@ -8,103 +8,102 @@ import './movie-view.scss';
 
 import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 
-class MovieView extends React.Component {
-  render() {
-    const { user, userData, movie, onBackClick } = this.props;
-    const { currentList } = props.favoriteMovies;
+//class MovieView extends React.Component {
+function MovieView() {
+  //render() {
+  const { user, userData, movie, onBackClick } = this.props;
+  const { currentList, setCurrentList } = useState(props.favoriteMovies);
 
-    const addFavoriteMovie = (movie, userData) => {
-      const token = localStorage.getItem('token');
-      const addedMovie = favoriteMovies.filter((m) =>
-        m._id.includes(movie._id)
-      );
-      console.log(addedMovie.length);
-      if (addedMovie.length > 0) {
-        alert('Movie is already a favorite.');
-        return;
-      } else {
-        axios
-          .post(
-            `https://ryan-viers-movie-app.herokuapp.com/users/${user}/movies/${movie._id}`,
-            {
-              FavoriteMovies: movie._id,
-            },
-            {
-              headers: { Authorization: `Bearer ${token}` },
-            }.then(() => {
-              const newList = currentList.add(movie);
-              this.props.setFavorite(newList);
-            })
-          )
-          .catch((e) => {
-            console.error(e);
-            alert('Unable to add movie to list.');
-          });
-      }
-    };
+  const addFavoriteMovie = (movie, userData) => {
+    const token = localStorage.getItem('token');
+    const addedMovie = favoriteMovies.filter((m) => m._id.includes(movie._id));
+    console.log(addedMovie.length);
+    if (addedMovie.length > 0) {
+      alert('Movie is already a favorite.');
+      return;
+    } else {
+      axios
+        .post(
+          `https://ryan-viers-movie-app.herokuapp.com/users/${user}/movies/${movie._id}`,
+          {
+            FavoriteMovies: movie._id,
+          },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }.then(() => {
+            const newList = currentList.add(movie);
+            this.props.setFavorite(newList);
+          })
+        )
+        .catch((e) => {
+          console.error(e);
+          alert('Unable to add movie to list.');
+        });
+    }
+  };
 
-    return (
-      <Container>
-        <Row>
-          <Col className="justify-content-md-center">
-            <Card id="movie-view">
-              <Card.Body>
-                <Card.Img
-                  id="movie-view-image"
-                  variant="top"
-                  src={movie.ImagePath}
-                />
-                <Card.Title id="movie-title" className="movie-title">
-                  {movie.Title}
-                </Card.Title>
-                <Card.Text id="movie-description" className="movie-description">
-                  {movie.Description}
-                </Card.Text>
-                <Card.Text id="movie-genre" className="movie-genre">
-                  Genre: {movie.Genre.Name}
-                </Card.Text>
-                <Card.Text id="movie-director" className="movie-director">
-                  Director: {movie.Director.Name}
-                </Card.Text>
+  return (
+    <Container>
+      <Row>
+        <Col className="justify-content-md-center">
+          <Card id="movie-view">
+            <Card.Body>
+              <Card.Img
+                id="movie-view-image"
+                variant="top"
+                src={movie.ImagePath}
+              />
+              <Card.Title id="movie-title" className="movie-title">
+                {movie.Title}
+              </Card.Title>
+              <Card.Text id="movie-description" className="movie-description">
+                {movie.Description}
+              </Card.Text>
+              <Card.Text id="movie-genre" className="movie-genre">
+                Genre: {movie.Genre.Name}
+              </Card.Text>
+              <Card.Text id="movie-director" className="movie-director">
+                Director: {movie.Director.Name}
+              </Card.Text>
 
-                <Button
-                  id="movie-view-button"
-                  onClick={() => {
-                    onBackClick();
-                  }}
-                >
-                  Back
+              <Button
+                id="movie-view-button"
+                onClick={() => {
+                  onBackClick();
+                }}
+              >
+                Back
+              </Button>
+
+              <Button
+                variant="secondary"
+                id="movie-view-button"
+                onClick={() => {
+                  addFavoriteMovie(movie, userData);
+                  alert('Added to Favorites');
+                }}
+              >
+                Add To Favorite Movies
+              </Button>
+
+              <Link to={`/directors/${movie.Director.Name}`}>
+                <Button variant="link" id="movie-view-button">
+                  Director
                 </Button>
+              </Link>
 
-                <Button
-                  variant="secondary"
-                  id="movie-view-button"
-                  onClick={() => {
-                    addFavoriteMovie(movie, userData);
-                    alert('Added to Favorites');
-                  }}
-                >
-                  Add To Favorite Movies
+              <Link to={`/genre/${movie.Genre.Name}`}>
+                <Button variant="link" id="movie-view-button">
+                  Genre
                 </Button>
-
-                <Link to={`/directors/${movie.Director.Name}`}>
-                  <Button variant="link" id="movie-view-button">
-                    Director
-                  </Button>
-                </Link>
-
-                <Link to={`/genre/${movie.Genre.Name}`}>
-                  <Button variant="link" id="movie-view-button">
-                    Genre
-                  </Button>
-                </Link>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+  //}
 }
 
 let mapStateToProps = (state) => {
